3aad9b15db194cd41bb48c6447c0ebe478ed3c57
Marie Blakely <marieblakely@myyahoo.com>
2024-04-09 19:24:04 -0400

    update redirect function

--- data/cars-data.js
+++ data/cars-data.js
@@ -1,7 +1,7 @@
 const cars = [
-  {text: 'Escalade', suv: true, _id: 125223},
-  {text: 'Range Rover', suv: false, _id: 127904},
-  {text: 'Tesla Model 3', suv: false, _id: 139608},
+  {make: 'Escalade', suv: true, _id: 125223},
+  {make: 'Range Rover', suv: true, _id: 127904},
+  {make: 'Tesla Model 3', suv: false, _id: 139608},
 ]
 
 export { 
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // import modules
 
 import express from 'express'
+import { cars } from './data/cars-data.js'
 
 // create Express app
 
@@ -16,12 +17,22 @@ app.set('view engine', 'ejs')
 
 // Mount routes
 
-app.get('/', function(req, res) {
-  res.send('<h1>hello, friend</h1>')
-})
+// app.get('/', function(req, res) {
+//   res.send('<h1>hello, friend</h1>')
+// })
 
 app.get('/home', function(req, res) {
   res.render('home')
+})
+
+app.get('/cars', function(req, res) {
+  res.render('cars/index', {
+    cars: cars
+  })
+})
+
+app.get('/', function(req, res) {
+  res.redirect('/home')
 })
 
 // tell the app to listen on port 3000
